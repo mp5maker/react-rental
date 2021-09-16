6466fb740214bd3cd4b86a13ebcd8f967c9facf6
mp5maker <khan.photon@gmail.com>
2021-09-16 21:33:02 +0600

    feat: add preliminary book details

--- src/components/common/book/index.tsx
+++ src/components/common/book/index.tsx
@@ -2,15 +2,41 @@ import * as React from 'react'
 import useSelect from '../../../hooks/useSelect'
 import bookSelect from '../../../utitlities/bookSelect'
 import Select from '../../select'
+import get from 'lodash/get'
 import './book.scss'
 
 interface IBookProps {
   rentals: Array<any>
 }
 
+const BookDetails = ({ item }: any): JSX.Element => {
+  const name = get(item, 'name', '')
+  const rentalPeriod = get(item, 'minimum_rent_period', '')
+  const mileage = get(item, 'mileage', 'N/A')
+  const needing_repair = get(item, 'needing_repair', false) ? 'Yes' : 'No'
+
+  return (
+    <div>
+      <div>
+        <h4>Name: {name}</h4>
+      </div>
+      <div>
+        <p>Rental Period: {rentalPeriod}</p>
+      </div>
+      <div>
+        <p>Mileage: {mileage}</p>
+      </div>
+      <div>
+        <p>Repair Needed: {needing_repair}</p>
+      </div>
+    </div>
+  )
+}
+
 const Book: React.FC<IBookProps> = ({ rentals }): JSX.Element => {
-  const { selected, handleSelect } = useSelect()
+  const { selected, handleSelect } = useSelect(rentals[0].code)
   const options = bookSelect({ data: rentals })
+  const selectedObj = rentals.find(rental => get(rental, 'code', '') === selected)
 
   return (
     <div className={'book-container'}>
@@ -19,6 +45,7 @@ const Book: React.FC<IBookProps> = ({ rentals }): JSX.Element => {
       </div>
       <div className={'book-content'}>
         <Select onChange={handleSelect} value={selected} options={options} />
+        <BookDetails item={selectedObj} />
       </div>
     </div>
   )
--- src/hooks/useSelect.tsx
+++ src/hooks/useSelect.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import get from 'lodash/get'
 
-const useSelect = () => {
-  const [selected, setSelected] = React.useState<string>('')
+const useSelect = (initial: string) => {
+  const [selected, setSelected] = React.useState<string>(initial ? initial : '')
 
   const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const value = get(event, 'target.value', '')
